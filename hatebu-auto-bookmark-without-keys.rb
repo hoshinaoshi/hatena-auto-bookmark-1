3dafbd5812c63aa7a082a5638da42cf552931ce6
NaoshiHoshi <hoshinaoshi@gmail.com>
2018-03-06 15:23:13 +0900

    仕様修正とruby-style-guideの適用
    - 意味をなしていないコメントの削除
    - インデントを半角スペース2つに修正
    - 複数の引数の間にスペースを挿入
    - シングルクォートではなくダブルクォートに変更
    - 不要な一時変数をやめる
    ref. https://github.com/fortissimo1997/ruby-style-guide/blob/japanese/README.ja.md

--- hatebu-auto-bookmark-without-keys.rb
+++ hatebu-auto-bookmark-without-keys.rb
@@ -2,33 +2,31 @@ require "hatena-bookmark"
 require "open-uri"
 require "nokogiri"
 
-for path in 0..9 do
-    multipled_path = path * 40
-    tag = "css"
-    url = "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&of=#{multipled_path}"
-    opt = {}
-    opt['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/XXXXXXXXXXXXX Safari/XXXXXX Vivaldi/XXXXXXXXXX' # user agent
-    charset = nil
+tags = %w(css)
+tags.each do |tag|
+  url = "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&users=50"
+  option = {
+    "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
+  }
 
-    html = open(url,opt) do |f|
-        charset = f.charset
-        f.read
-    end
+  html = open(url, option) do |f|
+    charset = f.charset
+    f.read
+  end
 
-    doc = Nokogiri::HTML.parse(html,nil,charset)
-    doc.css(".search-result h3 a").each do |a|
-        url = a[:href]
-        p url
-        sleep(1)
+  doc = Nokogiri::HTML.parse(html, nil, nil)
+  doc.css(".search-result h3 a").each do |a|
+    url = a[:href]
+    p url
 
-        hatebu = Hatena::Bookmark.new(
-          consumer_key:    '取得',
-          consumer_secret: '取得',
-          request_token:   '取得',  #access_token
-          request_secret:  '取得'  #access_token_secret
-        )
+    hatebu = Hatena::Bookmark.new(
+      consumer_key:    "取得",
+      consumer_secret: "取得",
+      request_token:   "取得",
+      request_secret:  "取得"
+    )
 
-        hatebu.create(:url => url)
-    end
-
+    hatebu.create(:url => url)
+    sleep(rand(100))
+  end
 end
