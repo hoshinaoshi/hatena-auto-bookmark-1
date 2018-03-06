e4ae983991bcde5b14e2e2a6dabefda9c7655e23
Ayumi Tamai <ayumi@Ayumis-MBP.local>
2018-03-07 02:02:09 +0900

    encode japanese tag keywords

--- hatebu-auto-bookmark-without-keys.rb
+++ hatebu-auto-bookmark-without-keys.rb
@@ -2,10 +2,10 @@ require "hatena-bookmark"
 require "open-uri"
 require "nokogiri"
 
-tags = %w(web プログラミング activerecord ai android api app ar aws bash batch bootstrap c# c++ chrome css dart database db deeplearning design development docker electron font frontend gcp git github go golang html html5 http https ibm ios java javascript jquery js json jvm kubernetes laravel linux monitoring mysql node.js performance php postgresql programming python qiita rails raspberrypi rds react ruby rust scala selenium sql ssl swift tcp tech techfeed technology typescript ubuntu ui uml unity ux vagrant virtualbox vmware vr vue vue.js webdesign webpack webアプリケーション webデザイン web制作 xcode エディタ エンジニア コンテナ サーバ システム ディープラーニング デザイン データ ドメイン フォント フリーソフト フロントエンド プラグイン プログラミング ライブラリ 人工知能 機械学習 深層学習 素材 設計 開発)
+tags = %w(activerecord ai android api app ar aws bash batch bootstrap c# c++ chrome css dart database db deeplearning design development docker electron font frontend gcp git github go golang html html5 http https ibm ios java javascript jquery js json jvm kubernetes laravel linux monitoring mysql node.js performance php postgresql programming python qiita rails raspberrypi rds react ruby rust scala selenium sql ssl swift tcp tech techfeed technology typescript ubuntu ui uml unity ux vagrant virtualbox vmware vr vue vue.js web webdesign webpack xcode webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム ディープラーニング デザイン データ ドメイン フォント フリーソフト フロントエンド プラグイン プログラミング ライブラリ 人工知能 機械学習 深層学習 素材 設計 開発)
 
 tags.each do |tag|
-  url = "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&users=50"
+  url = URI.encode "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&users=50"
   option = {
     "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
   }
