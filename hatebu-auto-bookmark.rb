c918aff34819b8ed2706d00649bd3c535831fe42
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-07 13:56:46 +0900

    リポジトリ移行

--- hatebu-auto-bookmark.rb
+++ hatebu-auto-bookmark.rb
@@ -1,8 +1,9 @@
 require "hatena-bookmark"
 require "open-uri"
 require "nokogiri"
+require "date"
 
-tags = %w(activerecord ai android api app ar aws bash batch bootstrap c# c++ chrome css dart database db deeplearning design development docker electron font frontend gcp git github go golang html html5 http https ibm ios java javascript jquery js json jvm kubernetes laravel linux monitoring mysql node.js performance php postgresql programming python qiita rails raspberrypi rds react ruby rust scala selenium sql ssl swift tcp tech techfeed technology typescript ubuntu ui uml unity ux vagrant virtualbox vmware vr vue vue.js web webdesign webpack xcode webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム ディープラーニング デザイン データ ドメイン フォント フリーソフト フロントエンド プラグイン プログラミング ライブラリ 人工知能 機械学習 深層学習 素材 設計 開発)
+tags = %w(activerecord agile ai android api app ar aws bash batch bootstrap c# c++ chrome css css3 dart database db deeplearning design development docker electron font frontend gcp git github go golang html html5 http https ibm ios java javascript jquery js json jvm kubernetes laravel linux monitoring mysql node.js performance php postgresql programming python qiita rails raspberrypi rds react ruby rust scala selenium sql ssl swift tcp tech techfeed technology typescript ubuntu ui uml unity ux vagrant virtualbox vmware vr vue vue.js web webdesign webpack xcode webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム ディープラーニング デザイン データ ドメイン フォント フリーソフト フロントエンド プラグイン プログラミング ライブラリ 人工知能 機械学習 深層学習 素材 設計 開発)
 
 tags.each do |tag|
   url = URI.encode "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&users=50"
@@ -16,25 +17,23 @@ tags.each do |tag|
   end
 
   doc = Nokogiri::HTML.parse(html, nil, nil)
-  doc.css(".search-result h3 a").each do |a|
-
-    hatebu = Hatena::Bookmark.new(
-      # consumer keyなど
-    )
-
-    feed =  hatebu.feed
-    bookmarks = feed["feed"]["entry"]
-    bookmarks.each do |bkm|
+  doc.css(".search-result blockquote .created").each do |day|
+    doc.css(".search-result h3 a").each do |a|
       url = a[:href]
-      already = bkm["link"][0]["href"]
-      if !already.include?(url)
-        hatebu.create(:url => url)
-        p "Successfully bookmarked: " + url
-      else
-        p "That url has been already bookmarked"
-      end
-    end
 
-    sleep(rand(100))
+      hatebu = Hatena::Bookmark.new(
+        #keys
+      )
+
+      today = Date.today.to_s.gsub("-","/")
+
+      if day = today
+          hatebu.create(:url => url)
+          p "bookmarked: " + url + " today is " +day
+      end
+
+      sleep(rand(100))
+
+    end
   end
 end
