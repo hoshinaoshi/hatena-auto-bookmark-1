3d311ae7a3539377047df3d8ff8b7026622487ca
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-07 11:57:36 +0900

    既にブクマされたページはブクマしない

--- hatebu-auto-bookmark.rb
+++ hatebu-auto-bookmark.rb
@@ -1,7 +1,6 @@
 require "hatena-bookmark"
 require "open-uri"
 require "nokogiri"
-require "json"
 
 tags = %w(activerecord ai android api app ar aws bash batch bootstrap c# c++ chrome css dart database db deeplearning design development docker electron font frontend gcp git github go golang html html5 http https ibm ios java javascript jquery js json jvm kubernetes laravel linux monitoring mysql node.js performance php postgresql programming python qiita rails raspberrypi rds react ruby rust scala selenium sql ssl swift tcp tech techfeed technology typescript ubuntu ui uml unity ux vagrant virtualbox vmware vr vue vue.js web webdesign webpack xcode webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム ディープラーニング デザイン データ ドメイン フォント フリーソフト フロントエンド プラグイン プログラミング ライブラリ 人工知能 機械学習 深層学習 素材 設計 開発)
 
@@ -18,26 +17,24 @@ tags.each do |tag|
 
   doc = Nokogiri::HTML.parse(html, nil, nil)
   doc.css(".search-result h3 a").each do |a|
-    url = a[:href]
 
     hatebu = Hatena::Bookmark.new(
-      consumer_key:    "fBXdaQtUu2jCig==",
-      consumer_secret: "YQATBkb/6A5fuox0iubAYNOrRA8=",
-      request_token:   "l20IwWq11VzFpQ==",
-      request_secret:  "Jbs/SgLGbZ190iSg+32848ah46w="
+      # consumer keyなど
     )
 
-    # hatebu.create(:url => url)
     feed =  hatebu.feed
-    # puts JSON.pretty_generate(feed)
-    bookmarks = feed["feed"]["entry"][0]["link"][0]["href"]
-    # puts JSON.pretty_generate(a)
-    # p "bookmarks: " + bookmarks
-    if !bookmarks.include?(url)
-      p "url bookmarked: " + url
+    bookmarks = feed["feed"]["entry"]
+    bookmarks.each do |bkm|
+      url = a[:href]
+      already = bkm["link"][0]["href"]
+      if !already.include?(url)
+        hatebu.create(:url => url)
+        p "Successfully bookmarked: " + url
+      else
+        p "That url has been already bookmarked"
+      end
     end
 
-    # sleep(rand(100))
-    sleep(1)
+    sleep(rand(100))
   end
 end
--- hatebu-getting-access-token.rb
+++ hatebu-getting-access-token.rb
@@ -8,8 +8,8 @@ SITE_URI          = 'https://www.hatena.com'
 REQUEST_TOKEN_URI = '/oauth/initiate?scope=read_public%2Cwrite_public'
 ACCESS_TOKEN_URI  = '/oauth/token'
 
-consumer = OAuth::Consumer.new('取得',
-                               '取得',
+consumer = OAuth::Consumer.new('fBXdaQtUu2jCig==',
+                               'YQATBkb/6A5fuox0iubAYNOrRA8=',
                                site: SITE_URI,
                                request_token_url: REQUEST_TOKEN_URI,
                                access_token_url: ACCESS_TOKEN_URI,
@@ -18,7 +18,10 @@ request_token = consumer.get_request_token
 puts "Visit this website and get the PIN: #{request_token.authorize_url}"
 
 
-# consumer.options.delete(:oauth_callback)
-# access_token = request_token.get_access_token(oauth_verifier: '取得')
-# puts "Access token: #{access_token.token}"
-# puts "Access token secret: #{access_token.secret}"
+consumer.options.delete(:oauth_callback)
+access_token = request_token.get_access_token(oauth_verifier: 'jhNGKwzAast1LuZhVfmfnszi')
+puts "Access token: #{access_token.token}"
+puts "Access token secret: #{access_token.secret}"
+
+# access token: l20IwWq11VzFpQ==
+# access token secret: Jbs/SgLGbZ190iSg+32848ah46w=
