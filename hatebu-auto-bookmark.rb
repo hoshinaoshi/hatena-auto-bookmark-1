5a54cee4522a3c76629c0e711a6632066906494a
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-07 14:16:51 +0900

    タグを追加

--- hatebu-auto-bookmark.rb
+++ hatebu-auto-bookmark.rb
@@ -3,7 +3,7 @@ require "open-uri"
 require "nokogiri"
 require "date"
 
-tags = %w(activerecord agile ai android api app ar aws bash batch bootstrap c# c++ chrome css css3 dart database db deeplearning design development docker electron font frontend gcp git github go golang html html5 http https ibm ios java javascript jquery js json jvm kubernetes laravel linux monitoring mysql node.js performance php postgresql programming python qiita rails raspberrypi rds react ruby rust scala selenium sql ssl swift tcp tech techfeed technology typescript ubuntu ui uml unity ux vagrant virtualbox vmware vr vue vue.js web webdesign webpack xcode webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム ディープラーニング デザイン データ ドメイン フォント フリーソフト フロントエンド プラグイン プログラミング ライブラリ 人工知能 機械学習 深層学習 素材 設計 開発)
+tags = %w(activerecord agile ai android api app ar aws bash batch bootstrap c# c++ cakephp ci chrome css css3 dart database db deeplearning design dev development dns docker electron font frontend gas gcp git github go golang haskell html html5 http https ibm ide ios java javascript jquery js json jvm kubernetes laravel linux machinelearning macos monitoring mysql node.js npm nuxt.js oracle oss performance php postgresql programming python qiita rails raspberrypi rds react ruby rust scala selenium shell sql ssl swift tcp tech techfeed technology tensorflow typescript ubuntu ui uml unity ux vagrant virtualbox vmware vr vue vue.js web webdesign webpack xcode webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム ディープラーニング デザイン データ ドメイン フォント フリーソフト フロントエンド プラグイン プログラミング ライブラリ 人工知能 機械学習 深層学習 素材 設計 開発)
 
 tags.each do |tag|
   url = URI.encode "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&users=50"
