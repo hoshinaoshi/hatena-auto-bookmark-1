14639d045dec9287563d0dab403af3e511f38f43
ayumitamai97 <ayumitamai97@gmail.com>
2018-06-12 02:09:28 +0900

    引数でアカウントを条件分岐するように変更

--- hatebu-auto-bookmark.rb
+++ hatebu-auto-bookmark.rb
@@ -1,40 +1,62 @@
-# 使用しない
-
 require "hatena-bookmark"
 require "open-uri"
 require "nokogiri"
 require "date"
+require "pry"
+require "json"
 
-tags = %w(activerecord aws bootstrap css css3 database db design dev development docker font frontend git github html html5 javascript js linux macos mysql node.js npm postgresql programming qiita rails react ruby sql tech techfeed technology ubuntu ui ux vue vue.js web webdesign webpack webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム デザイン データ フォント フロントエンド プログラミング 設計 開発)
+creds = JSON.load(File.read("credentials.json"))
+
+# %w(activerecord aws bootstrap css css3 database db design dev development docker font frontend git github html html5 javascript js linux macos mysql node.js npm postgresql programming qiita rails react ruby sql tech techfeed technology ubuntu ui ux vue vue.js web webdesign webpack webアプリケーション webデザイン web制作 エディタ エンジニア コンテナ サーバ システム デザイン データ フォント フロントエンド プログラミング 設計 開発)
+
+arg = ARGV[0]
+tags =
+  if arg == "adele"
+    %w(activerecord aws dev ラーメン development 歴史 docker git github 犯罪 linux 雑談 これはひどい ジェンダー mysql node.js npm programming qiita rails react ruby sql vue vue.js ネット webアプリケーション ネタ webデザイン web制作 エディタ エンジニア 大阪 東京 人生 コンテナ サーバ プログラミング 設計 開発)
+  elsif arg == "oyasumi"
+    %w(activerecord aws dev ラーメン development 歴史 docker git github 犯罪 linux 雑談 これはひどい ジェンダー mysql node.js npm programming qiita rails react ruby sql vue vue.js ネット webアプリケーション ネタ webデザイン web制作 エディタ エンジニア 大阪 東京 人生 コンテナ サーバ プログラミング 設計 開発)
+  elsif arg == "rhodia"
+    %w(database db カメラ design dev development docker オカルト frontend git github html html5 お金 javascript js linux macos qiita tech techfeed technology ubuntu ui ux vue vue.js web webdesign webpack webアプリケーション 海外 webデザイン web制作 エディタ エンジニア コンテナ サーバ システム コーヒー デザイン フロントエンド プログラミング 設計 開発)
+  end
 
 tags.each do |tag|
   p "Starting to get: " + tag
-  url = URI.encode "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&users=3"
+  url = URI.encode "http://b.hatena.ne.jp/search/tag?safe=on&q=#{tag}&users=20"
   # タグ1つあたり平均1件ほどブクマされるように絞り込み
 
-  html = open(url, option) do |f|
+  html = open(
+    url,
+    {"User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"}
+    # はてブのページはUserAgentを偽装しないと取得できない
+    ) do |f|
     charset = f.charset
     f.read
+    # 文字化け回避
   end
 
   doc = Nokogiri::HTML.parse(html, nil, nil)
-  doc.css(".search-result blockquote .created").each do |day|
-    doc.css(".search-result h3 a").each do |a|
-      url = a[:href]
 
-      hatebu = Hatena::Bookmark.new(
-        #keys
-      )
+  hatebu = Hatena::Bookmark.new(
+    consumer_key:    creds[arg]["consumer_key"],
+    consumer_secret: creds[arg]["consumer_secret"],
+    request_token:   creds[arg]["Access token"],
+    request_secret:  creds[arg]["Access token secret"]
+  )
 
-      today = Date.today.to_s.gsub("-","/")
+  for num in 0..9 # タグ1つあたりの新着記事は多くの場合せいぜい3件程度であるため
+    created_at = doc.css(".entry-contents-date")[num].inner_text.gsub(" ", "")
+    latest_provisional = Date.today - 1
+    latest = latest_provisional.to_s.gsub("-","/")
 
-      if day = today
-          hatebu.create(:url => url)
-          p "bookmarked: " + url + " today is " +day
-      end
-
-      sleep(rand(100))
-
+    if created_at == latest
+      # 今日のエントリーを取得しても今日中にまた更新される可能性があるため、前日更新のエントリーをブクマする
+      hatebu.create(url: doc.css(".bookmark-item h3 a")[num][:href])
+      p "Bookmarked: " + doc.css(".bookmark-item h3 a")[num][:href]
+    else
+      puts "Not Bookmarked: " + doc.css(".bookmark-item h3 a")[num][:href]
     end
+
+    sleep(rand(10))
   end
+
 end
